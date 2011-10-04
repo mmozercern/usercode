e16ce7c23a8e8a59588ca94f0895a0499f329a16
mmozer <>
2011-10-04 09:42:27 +0000

    lsf submission scripts

---
+++ HiggsCards/scripts/parallelizeCombine.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+NMAXJOBS=50
+mass=$1
+queue="dummyqueue"
+OUTDIR="./"
+
+if [ $# -gt 1 ]
+then
+    queue=$2
+else
+    queue=8nh
+fi
+
+ijob=1
+chmod 744 combine_exec.sh
+
+while [ $ijob -le $NMAXJOBS ]
+do
+  myrand=$RANDOM #random number generator (short integer: [0-32767])
+  JOBNAME="combine_${myrand}"
+  LOGFILE="log_batch_combine_${myrand}.out"
+  bsub -q $queue -J $JOBNAME -oo ${OUTDIR}/${mass}/$LOGFILE combine_exec.sh $myrand $mass
+  let ijob=ijob+1
+done
+
+
