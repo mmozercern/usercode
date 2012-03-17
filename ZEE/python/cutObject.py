5031abd26f0c94777ffe43debc1411d7b48ab537
mmozercern <>
2012-03-17 14:16:29 +0000

    This commit was manufactured by cvs2svn to create tag 'logger_mmozer_12Jul12-13h21m07s'.
    Sprout from master 2012-03-17 14:16:28 UTC bonato 'Update scripts and macros for limits with MCMC and Asymptotic CLs'
    Delete:
        ElectronTagProbe/3deffMC_sc.py
        ElectronTagProbe/3deffMC_sel.py
        ElectronTagProbe/3deffX_PU_sc.py
        ElectronTagProbe/3deffX_PU_sel.py
        ElectronTagProbe/3deffX_PU_trigger.py
        ElectronTagProbe/3deffX_sc.py
        ElectronTagProbe/3deffX_sel.py
        ElectronTagProbe/3deffX_trigger.py
        ElectronTagProbe/tp_ele_cfg.py
        MuonTagProbe/3d_data_coarse_final.py
        MuonTagProbe/3d_data_final.py
        MuonTagProbe/tagprobeproducer_all_cfg.py
        Oct09Skims/HEEPonePAT_cfg.py
        Oct09Skims/HEEPone_cfg.py
        Oct09Skims/HEEPtwoPAT_cfg.py
        Oct09Skims/HEEPtwo_cfg.py
        ZEE/BuildFile.xml
        ZEE/interface/EffAggregator.h
        ZEE/interface/SinPhiMETMHT.h
        ZEE/interface/VecFromView.h
        ZEE/python/VBSequence_cff.py
        ZEE/python/VBcuts.py
        ZEE/python/VBcuts_old.py
        ZEE/python/cutObject.py
        ZEE/python/surgery.py
        ZEE/src/EffAggregator.cc
        ZEE/src/SinPhiMETMHT.cc
        ZEE/src/VecFromView.cc
        ZEE/test/MM_PF_cfg.py
        ZEE/test/MM_Zee_cfg.py
        emucombine/RooEBackground.cxx
        emucombine/RooEBackground.h
        emucombine/RooEBackground_cxx.so
        emucombine/RooMuBackground.cxx
        emucombine/RooMuBackground.h
        emucombine/RooMuBackground_cxx.so
        emucombine/classes/DataSetHelper.cxx
        emucombine/classes/DataSetHelper.h
        emucombine/classes/HEEPStyle_noTitle.C
        emucombine/classes/Model.cxx
        emucombine/classes/Model.h
        emucombine/classes/PaperPlot.txt
        emucombine/classes/README.txt
        emucombine/classes/ResultCollection.cxx
        emucombine/classes/ResultCollection.h
        emucombine/classes/RooEBackground.cxx
        emucombine/classes/RooEBackground.h
        emucombine/classes/RooMuBackground.cxx
        emucombine/classes/RooMuBackground.h
        emucombine/classes/TreeLoader.cxx
        emucombine/classes/TreeLoader.h
        emucombine/classes/correspond.C
        emucombine/classes/crab.cfg
        emucombine/classes/deltam.C
        emucombine/classes/e_0_3.txt
        emucombine/classes/e_40_43.txt
        emucombine/classes/fwlitegridtest.sh
        emucombine/classes/hadd.C
        emucombine/classes/limit.C
        emucombine/classes/loadlib.C
        emucombine/classes/lookelse.C
        emucombine/classes/lookelsewhere.C
        emucombine/classes/merge.C
        emucombine/classes/opt.C
        emucombine/classes/papersig.C
        emucombine/classes/plotnll.C
        emucombine/classes/test.C
        emucombine/combine.C
        emucombine/cumulative.C
        emucombine/lookelsewhere.C
        gravFit/Makefile
        gravFit/SidebandFitter.cc
        gravFit/SidebandFitter.h
        gravFit/fitSidebands.cpp

--- ZEE/python/cutObject.py
+++
@@ -1,8 +0,0 @@
-class cutObject:
-    def __init__(self, order, label, type , tight, loose, n):
-        self.order = order
-        self.label = label
-        self.type  = type
-        self.tight = tight
-        self.loose = loose
-        self.n     = n
--- emucombine/classes/loadlib.C
+++
@@ -1,9 +0,0 @@
-
-{
-  gSystem->Load("RooMuBackground_cxx.so");
-  gSystem->Load("RooEBackground_cxx.so");
-  gSystem->Load("TreeLoader_cxx.so");
-  gSystem->Load("DataSetHelper_cxx.so");
-  gSystem->Load("ResultCollection_cxx.so");
-  gSystem->Load("Model_cxx.so");
-}
